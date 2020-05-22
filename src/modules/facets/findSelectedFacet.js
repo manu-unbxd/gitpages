bb6ed13715f955d81882adfa961c2453d912670a
Manu Sunny <manu.sunny@unbxd.com>
2020-05-22 10:32:14 +0530

    docs update

--- src/modules/facets/findSelectedFacet.js
+++ src/modules/facets/findSelectedFacet.js
@@ -2,7 +2,8 @@
 * @description 
 * to get the selected facet values
 * accepts a string
-* @param {string} parent eg:color_facet 
+* @param {string} parent eg:color_facet
+* @method findSelectedFacet
 */
 export default function(selected) {
     //items.findIndex(x => x.id == item.id);
